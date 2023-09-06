3e43a135611a083dfed68ce2075acc3354cad374
empeo <124434139+empeo@users.noreply.github.com>
2023-09-06 17:09:15 +0300

    Create dynamic.js

---
+++ Sprint2/Cubic_Shadow/js/dynamic.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".btn_to_location");
+    let btn_all = document.querySelector(".container_cubic");
+    btn.addEventListener("click",()=>{
+        location.assign("https://www.facebook.com/Dragons.Egypt");
+    });
+    btn_all.addEventListener("click",()=>{
+        location.assign("https://www.facebook.com/Dragons.Egypt");
+    });
